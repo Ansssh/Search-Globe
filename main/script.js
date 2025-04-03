80010c51c4ae08bf2df872ca62bc0a31003d598f
Anshh <anshkumar975310@gmail.com>
2025-04-03 17:53:03 +0530

    Started Working on JS

--- main/script.js
+++ main/script.js
@@ -10,7 +10,7 @@ changeDisplay.addEventListener("click", () => {
         elements.forEach((ele) => {
             ele.classList.add("dark-mode");
         })
-        displayModeSvg.innerHTML = `<path d="M12 3V4M12 20V21M4 12H3M6.31412 6.31412L5.5 5.5M17.6859 6.31412L18.5 5.5M6.31412 17.69L5.5 18.5001M17.6859 17.69L18.5 18.5001M21 12H20M16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>`;
+        displayModeSvg.innerHTML = `<path d="M12 3V4M12 20V21M4 12H3M6.31412 6.31412L5.5 5.5M17.6859 6.31412L18.5 5.5M6.31412 17.69L5.5 18.5001M17.6859 17.69L18.5 18.5001M21 12H20M16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12Z"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>`;
         displayModeText.textContent = "Light Mode";
         isDarkMode = true;
     } else {
@@ -22,3 +22,22 @@ changeDisplay.addEventListener("click", () => {
         isDarkMode = false
     }
 })
+
+
+
+async function getData() {
+    const url = "https://restcountries.com/v3.1/all";
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${data.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch(error) {
+        console.error("Error fetching data:", error);
+        return null;
+    }
+}
